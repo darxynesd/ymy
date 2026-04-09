4ca73dbd2b1d42d98fa6c33a8a01ed47b0e75d73
darxynesd <20yanapa08@gmail.com>
2026-04-09 12:52:20 +0300

    check_same_thread=False Allows access from different flows (needed for FastAPI) autocommit=False Self-monitoring of comites autoflush=False Does not automatically request the database

--- database.py
+++ database.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./test.db"
 
-engine= create_engine(DATABASE_URL)
-SessionLocal= sessionmaker(bind=engine)
+engine= create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
+SessionLocal= sessionmaker(bind=engine,autocommit=False,autoflush=False)
 Base= declarative_base()
